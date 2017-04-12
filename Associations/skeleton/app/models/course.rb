57e76cdbf4add926e9d4c2f75809c578ebbd6383
App Academy Student <student@appacademy.io>
2017-04-12 11:18:28 -0700

    Creates Enrollment, User, and Course models

---
+++ Associations/skeleton/app/models/course.rb
@@ -0,0 +1,21 @@
+class Course < ActiveRecord::Base
+  has_many(
+    :enrollments,
+    class_name: "Enrollment",
+    foreign_key: :course_id,
+    primary_key: :id
+  )
+
+  has_many(
+    :enrolled_students,
+    through: :enrollments,
+    source: :user
+  )
+
+  belongs_to(
+    :instructor,
+    class_name: "User",
+    foreign_key: :instructor_id,
+    primary_key: :id
+  )
+end
